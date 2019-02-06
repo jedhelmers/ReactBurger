7313dce578e6ef8e0f24453202f6935e3501089f
Jed Helmers <jedhelmers@gmail.com>
2019-02-06 07:45:28 -0500

    HOC and other components

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Burger.css'
+import BurderIngredient from './BurgerIngredients/BurgerIngredients'
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurderIngredient key={igKey + i} type={igKey} />
+      })
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+
+    if(transformedIngredients.length === 0){
+      transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
+  console.log(transformedIngredients)
+    return (
+      <div className="Burger">
+        <BurderIngredient type="bread-top" />
+        {transformedIngredients}
+        <BurderIngredient type="bread-bottom" />
+      </div>
+    )
+}
+
+export default burger
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import classes from './Layout.css'
+import Aux from '../../hoc/Aux'
+
+const layout = (props) => (
+  // Aux is a higher order component HOC
+  // they are used to render components WITHOUT
+  // a parent div
+  // HOCs can take Classes... I guess to style children
+  // but if I don't like scoping CSS... is this really
+  // neccessary?
+  <Aux>
+    <div>
+      Toolbar, SideDrawer, Backdrop
+    </div>
+    <main className="Content">
+      {props.children}
+    </main>
+  </Aux>
+)
+
+export default layout
